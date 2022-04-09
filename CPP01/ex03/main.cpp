88df8813605fe33e11f86ee0f2f7040d31a6fc8d
Kang Jisoon <wltnskjs@naver.com>
2022-04-09 23:10:52 +0900

    🚧 Add main.c

--- CPP01/ex03/main.cpp
+++ CPP01/ex03/main.cpp
@@ -6,13 +6,33 @@
 /*   By: devleo <devleo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 17:33:03 by devleo            #+#    #+#             */
-/*   Updated: 2022/04/04 17:34:23 by devleo           ###   ########.fr       */
+/*   Updated: 2022/04/09 23:10:28 by devleo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include "Weapon.hpp"
+#include "HumanA.hpp"
+#include "HumanB.hpp"
 
-int main(void)
+int main()
 {
-    return(EXIT_SUCCESS);
+    {
+        Weapon  club = Weapon("crude spiked club");
+
+        HumanA bob("Bob", club);
+        bob.attack();
+        club.setType("some other type of club");
+        bob.attack();
+    }
+    {
+        Weapon  club = Weapon("crude spiked club");
+
+        HumanB jim("Jim");
+        jim.setWeapon(club);
+        jim.attack();
+        club.setType("some other type of club");
+        jim.attack();
+    }
+    return 0; 
 }
