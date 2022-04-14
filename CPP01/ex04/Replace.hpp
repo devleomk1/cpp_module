056ec511cf0f46f5e014dc23c0e090c032464752
Kang Jisoon <wltnskjs@naver.com>
2022-04-14 23:43:27 +0900

    🚧 Update replace.{cpp, hpp}

--- CPP01/ex04/Replace.hpp
+++ CPP01/ex04/Replace.hpp
@@ -1,3 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Replace.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/14 23:42:12 by jisokang          #+#    #+#             */
+/*   Updated: 2022/04/14 23:42:46 by jisokang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLOR_HPP
+# define COLOR_HPP
+
 class Replace
 {
 private:
@@ -7,10 +22,4 @@ public:
 	~Replace();
 };
 
-Replace::Replace(/* args */)
-{
-}
-
-Replace::~Replace()
-{
-}
+#endif
