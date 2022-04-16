39a255909ada79f8b262684671d191d6cd106f75
Kang Jisoon <wltnskjs@naver.com>
2022-04-16 23:09:55 +0900

    ⚡️ New replace code

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/11 03:29:47 by jisokang          #+#    #+#             */
-/*   Updated: 2022/04/15 23:15:48 by jisokang         ###   ########.fr       */
+/*   Updated: 2022/04/16 23:09:17 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,16 @@
 #include <iostream>
 #include "color.hpp"
 
-std::string::size_type replace(std::string str, std::string s1, std::string s2)
+std::string::size_type replace(std::string &str, std::string s1, std::string s2, std::string::size_type &n)
 {
-	std::string::size_type n;
-
-	n = str.find(s1);
+	n = str.find(s1, n);
 	if (n != std::string::npos)
 	{
 		str.erase(n, s1.length());
 		str.insert(n, s2);
-		replace(str, s1, s2);
-		std::cout << str << std::endl;
+		n += s1.length();
+		n = replace(str, s1, s2, n);
+		// std::cout << str << std::endl;
 	}
 	return(n);
 }
@@ -42,13 +41,15 @@ int main(int argc, char **argv)
 	std::string str;
 	const std::string s1 = argv[2];
 	const std::string s2 = argv[3];
+	std::string::size_type n = 0;
 
 	if (in.is_open())
 	{
 		while (in)
 		{
+			n = 0;
 			getline(in, str);
-			replace(str, s1, s2);
+			n = replace(str, s1, s2, n);
 			std::cout << str << std::endl;
 		}
 	}
