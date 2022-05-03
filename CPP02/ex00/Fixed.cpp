72a4c916eb604809d21c1d2a86aa896f60dc54b7
Kang Jisoon <wltnskjs@naver.com>
2022-05-04 01:45:30 +0900

    💡Update CPP02

--- CPP02/ex00/Fixed.cpp
+++ CPP02/ex00/Fixed.cpp
@@ -6,26 +6,52 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 22:02:00 by jisokang          #+#    #+#             */
-/*   Updated: 2022/05/02 21:32:16 by jisokang         ###   ########.fr       */
+/*   Updated: 2022/05/03 22:40:31 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-Fixed::Fixed(const Fixed &src)
-{
 
+Fixed::Fixed( void ) : fixed_num( 0 )		//생성자에서 fixed_num을 0으로 초기화
+{
+	std::cout << "Default constructor called\n";
 }
 
-Fixed &Fixed::operator=(const Fixed &src)
+Fixed::Fixed(const Fixed &f)
+{
+	//deep copy
+	std::cout << "Copy constructor called\n";
+
+	// this->fixed_num = f.fixed_num;
+	// 이건 왜 안될까?
+
+	*this = f;
+}
+
+Fixed &Fixed::operator=(const Fixed &f)
 {
 	//'='를 내 마음대로 다시 정의 하는 작업
-}
-
-Fixed::Fixed(/* args */)
-{
+	//'+', '-', '<' 등 다양한 오퍼레이터를 정의 할 수 있다.
+	std::cout << "Copy assignment operator called\n";
+	// ?????
+	if(this != &f)
+		fixed_num = f.getRawBits();
+	return (*this);
 }
 
 Fixed::~Fixed()
 {
+	std::cout << "Destructor called\n";
+}
+
+int Fixed::getRawBits( void ) const
+{
+	std::cout << BLUE "getRawBits" RESET " member function called\n";
+	return (this->fixed_num);
+}
+
+void Fixed::setRawBits( int const raw )
+{
+	this->fixed_num = raw;
 }
--- CPP02/ex00/Fixed.hpp
+++ CPP02/ex00/Fixed.hpp
@@ -6,12 +6,15 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 22:01:54 by jisokang          #+#    #+#             */
-/*   Updated: 2022/05/02 21:29:17 by jisokang         ###   ########.fr       */
+/*   Updated: 2022/05/03 22:32:52 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_CPP
 # define FIXED_CPP
+
+# include <iostream>
+# include "color.hpp"
 
 class Fixed
 {
@@ -21,9 +24,12 @@ private:
 public:
 	//Orthodox Canonical Form
 	Fixed();								//Default constructor
-	Fixed(const Fixed &src);				//Copy constructor
-	Fixed &operator=(const Fixed &src);		//Copy Asginment operator
+	Fixed(const Fixed &f);				//Copy constructor
+	Fixed &operator=(const Fixed &f);		//Copy Asginment operator
 	~Fixed();								//Destructor
+
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
 };
 
 #endif
