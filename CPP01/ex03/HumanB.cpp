751c6be8ebf6c32c10b5e68e62e5d56511efe06c
Kang Jisoon <wltnskjs@naver.com>
2022-05-04 21:53:33 +0900

    🩹Fix Human B has no weapon issue

--- CPP01/ex03/HumanB.cpp
+++ CPP01/ex03/HumanB.cpp
@@ -3,17 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   HumanB.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jisokang <jisokang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 18:46:11 by jisokang          #+#    #+#             */
-/*   Updated: 2022/04/10 19:45:51 by jisokang         ###   ########.fr       */
+/*   Updated: 2022/05/04 20:50:30 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
-HumanB::HumanB( std::string new_name ) : name(new_name)
+HumanB::HumanB( std::string new_name ) : name(new_name), weapon_type(NULL)
 {
+	
 }
 
 void HumanB::setWeapon( Weapon &new_weapon_type )
@@ -23,7 +24,12 @@ void HumanB::setWeapon( Weapon &new_weapon_type )
 
 void HumanB::attack()
 {
-	std::cout << MAGENTA << this->name << RESET << " attacks with their " << CYAN << this->weapon_type->getType() << RESET << std::endl;
+
+	std::cout << MAGENTA << this->name << RESET << " attacks with their " CYAN;
+	if (this->weapon_type != NULL)
+		std::cout <<  this->weapon_type->getType() << RESET << std::endl;
+	else
+		std::cout << "No Weapon" << RESET << std::endl;
 }
 
 HumanB::~HumanB()
