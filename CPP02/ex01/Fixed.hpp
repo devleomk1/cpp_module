a7c7ca4ce948f3ce95ac3fe3adf5ab5da0476be7
Kang Jisoon <wltnskjs@naver.com>
2022-05-05 17:29:44 +0900

    ✅ Add CPP02/ex01

---
+++ CPP02/ex01/Fixed.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/24 22:01:54 by jisokang          #+#    #+#             */
+/*   Updated: 2022/05/03 22:32:52 by jisokang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_CPP
+# define FIXED_CPP
+
+# include <iostream>
+# include "color.hpp"
+
+class Fixed
+{
+private:
+	int fixed_num;
+	static const int num_bit = 8;
+public:
+	//Orthodox Canonical Form
+	Fixed();								//Default constructor
+	Fixed(const Fixed &f);				//Copy constructor
+	Fixed &operator=(const Fixed &f);		//Copy Asginment operator
+	~Fixed();								//Destructor
+
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+};
+
+#endif
