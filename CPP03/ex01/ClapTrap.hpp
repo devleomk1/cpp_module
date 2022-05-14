c559bb3a39a0529723a28f59c539df48219860b3
Kang Jisoon <wltnskjs@naver.com>
2022-05-14 22:50:07 +0900

    ✅ Add CPP03/ex01

---
+++ CPP03/ex01/ClapTrap.hpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jisokang <jisokang@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/09 19:07:27 by jisokang          #+#    #+#             */
+/*   Updated: 2022/05/14 22:37:38 by jisokang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_CPP
+# define CLAPTRAP_CPP
+
+# include <iostream>
+# include <iomanip>      // std::setw
+# include "color.hpp"
+
+# define CLAP_DF_NAME	"NONAME"
+# define CLAP_HP		10
+# define CLAP_EP		10
+# define CLAP_AD		0
+# define CLAP_MAX_HP	1000
+
+class ClapTrap
+{
+private:
+
+protected:
+	std::string		_name;
+	unsigned int	_hp;							//Hit point
+	unsigned int	_ep;							//Energy point
+	unsigned int	_ad;							//Attaack damage
+
+public:
+	ClapTrap( void );								//Default constructor
+	ClapTrap( std::string name );					//Name constructor
+	ClapTrap( const ClapTrap &src );				//Copy constructor
+	ClapTrap &operator=( const ClapTrap &src );		//Copy Asginment operator
+	~ClapTrap();									//Destructor
+
+	//Add the following public member functions so the ClapTrap looks more realistic:
+	void	attack( const std::string &target );	//ClapTrap Attack target
+	void	takeDamage( unsigned int amount );		//ClapTrap take Damege
+	void	beRepaired( unsigned int amount );
+	void	setAd( unsigned int amount );
+	void	printStatus( void );
+};
+
+#endif
---
+++ CPP03/ex01/ScavTrap.cpp
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jisokang <jisokang@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/14 21:03:39 by jisokang          #+#    #+#             */
+/*   Updated: 2022/05/14 22:42:16 by jisokang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ScavTrap.hpp"
+
+ScavTrap::ScavTrap( void ) : ClapTrap()
+{
+	_hp = SCAV_HP;
+	_ep = SCAV_EP;
+	_ad = SCAV_AD;
+	std::cout
+		<< "[ScavTrap]: Scav! IN\t\t\t[Child " GREEN "Constructor" RESET "]"
+		<< std::endl;
+}
+
+//부모가 있어야만 자식을 만들 수 있어!
+ScavTrap::ScavTrap( std::string name) : ClapTrap(name)
+{
+	_hp = SCAV_HP;
+	_ep = SCAV_EP;
+	_ad = SCAV_AD;
+	std::cout
+		<< "[ScavTrap]: Scav! IN\t\t\t[Child " GREEN "Constructor" RESET "]"
+		<< std::endl;
+}
+
+ScavTrap::~ScavTrap()
+{
+	std::cout
+		<< "[ScavTrap]: Scav! OUT\t\t\t[Child " RED "Destructor" RESET "]"
+		<< std::endl;
+}
+
+ScavTrap &ScavTrap::operator=( const ScavTrap &src )
+{
+	std::cout << YELLOW "Copy" RESET " assignment operator called" << std::endl;
+	_name = src._name;
+	_hp = src._hp;
+	_ep = src._ep;
+	_ad = src._ad;
+
+	return (*this);
+}
+
+ScavTrap::ScavTrap( const ScavTrap &src )
+{
+	std::cout << YELLOW "Copy" RESET " constructor called" << std::endl;
+	*this = src;
+}
+
+void ScavTrap::guardGate()
+{
+	std::cout
+			<< "SlapTrap "  MAGENTA 
+			<< _name << RESET " Gate keeper mode ON!"
+			<< std::endl;
+	
+}
