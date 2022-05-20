e8512d9ed00092cf976baa928ccca3a6c7e117d3
Kang Jisoon <wltnskjs@naver.com>
2022-05-20 21:17:03 +0900

    🚧 Work in progress

--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -6,7 +6,41 @@
 /*   By: jisokang <jisokang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 14:29:08 by jisokang          #+#    #+#             */
-/*   Updated: 2022/05/19 14:29:09 by jisokang         ###   ########.fr       */
+/*   Updated: 2022/05/20 16:29:15 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include "color.hpp"
+
+class Bureaucrat
+{
+private:
+    const std::string   name;
+    unsigned int        grade; //ranges from 1 (highest possible grade) to 150 (lowest possible grade).
+public:
+    Bureaucrat( void );
+    Bureaucrat( const Bureaucrat &src );
+    Bureaucrat &operator=( const Bureaucrat &src );
+    virtual ~Bureaucrat( void );
+
+    // Bureaucrat::GradeTooHighException 
+    // Bureaucrat::GradeTooLowException.
+
+    std::string getName();
+    unsigned int getGrade();
+
+    void increGrade();
+    void decreGrade();
+    // If the grade is out of range, both of them will throw the same exceptions as the constructor.
+
+    /* 
+    Remember. Since grade 1 is the highest one and 150 the lowest, 
+    incrementing a grade 3 should give a grade 2 to the bureaucrat.
+    */
+};
+
+#endif
--- CPP05/ex00/color.hpp
+++ CPP05/ex00/color.hpp
@@ -1,21 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*   color.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jisokang <jisokang@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/19 14:29:04 by jisokang          #+#    #+#             */
-/*   Updated: 2022/05/20 16:23:53 by jisokang         ###   ########.fr       */
+/*   Created: 2022/01/20 00:59:06 by jisokang          #+#    #+#             */
+/*   Updated: 2022/03/11 14:12:41 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Bureaucrat.hpp"
+#ifndef COLOR_HPP
+# define COLOR_HPP
 
-Bureaucrat::Bureaucrat()
-{
-}
+# define RED		"\x1b[31m"
+# define GREEN		"\x1b[32m"
+# define BLUE		"\x1b[34m"
+# define YELLOW		"\x1b[33m"
+# define MAGENTA	"\x1b[35m"
+# define CYAN		"\x1b[36m"
 
-Bureaucrat::~Bureaucrat()
-{
-}
+# define RESET		"\x1b[0m"
+
+#endif
