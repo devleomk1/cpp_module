77df1707e554eb9a73502760630aa9398035766d
Kang Jisoon <wltnskjs@naver.com>
2022-05-30 17:32:37 +0900

    ⚰️ Remove dead code CPP06

--- CPP06/ex02/main.cpp
+++ CPP06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 11:43:23 by jisokang          #+#    #+#             */
-/*   Updated: 2022/05/30 15:35:57 by jisokang         ###   ########.fr       */
+/*   Updated: 2022/05/30 17:30:56 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 int	main(void)
 {
 	Base *base = generate();
-	identify( base );
-	identify( *base );
+	identify(base);
+	identify(*base);
 
 	delete(base);
 	return (0);
