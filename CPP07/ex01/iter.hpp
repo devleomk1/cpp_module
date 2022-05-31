c7647f2a75f561bfc626073fbb004f4cc22550f3
Kang Jisoon <wltnskjs@naver.com>
2022-05-31 22:25:11 +0900

    ✅ Update CPP07

---
+++ CPP07/ex01/iter.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/31 21:11:45 by jisokang          #+#    #+#             */
+/*   Updated: 2022/05/31 21:59:48 by jisokang         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+# define ITER_HPP
+
+# include <iostream>
+# include <string>
+
+template <typename T>
+template <typename A>
+template <typename B>
+void iter()
+{
+
+}
+
+#endif
