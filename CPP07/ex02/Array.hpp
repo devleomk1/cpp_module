5234f66815300add8b99bcdb8c5f1cec811aeb93
Kang Jisoon <wltnskjs@naver.com>
2022-06-01 20:15:31 +0900

    ✅ Done CPP07

--- CPP07/ex02/Array.hpp
+++ CPP07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 14:38:47 by jisokang          #+#    #+#             */
-/*   Updated: 2022/06/01 16:36:45 by jisokang         ###   ########.fr       */
+/*   Updated: 2022/06/01 20:09:07 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ public:
 	Array( void ) : _data(NULL), _len(0) {};
 
 	// Input constructor
-	Array( const int n ) : _data(NULL), _len(n) {};
+	Array( const unsigned int n ) : _data(new T[n]), _len(n) {};
 
 	//Copy constructor
 	/* 같은 타입만 받아와야 하니까 <T>를 넣어야 한다. */
@@ -41,14 +41,17 @@ public:
 	};
 
 	//Copy Asginment operator
-	Array &operator=( Array const &rhs )
+	Array<T> &operator=( const Array<T> &rhs )
 	{
 		if ( this != &rhs )
 		{
-			this->_len = rhs.getLen();
-			for (size_t i = 0; i < rhs.getLen() ; i++)
+			if (this->_len > 0)
+				delete[] _data;
+			this->_len = rhs._len;
+			this->_data = new T[rhs._len];
+			for (size_t i = 0; i < this->_len ; i++)
 			{
-				this->_data[i] = rhs.getData(i);
+				this->_data[i] = rhs._data[i];
 			}
 		}
 		return (*this);
@@ -60,22 +63,13 @@ public:
 		delete[] _data;
 	};
 
-	const unsigned int &getLen( void ) const
+	T &operator[]( const int n ) const
 	{
-		return (this->_len);
-	};
-	T &getData( const int n ) const
-	{
-		return (this->_data[n]);
-	};
-
-	T &operator[]( const int n )
-	{
-		if (n < 0 || n >= this->getLen())
+		if (n < 0 || static_cast<unsigned int>(n) >= this->_len)
 		{
 			throw WrongIndexException();
 		}
-		return (this->getData(n));
+		return (this->_data[n]);
 	};
 
 	class WrongIndexException : public std::exception
@@ -86,6 +80,18 @@ public:
 				return ("Wrong Index!");
 			}
 	};
+};
+
+template<typename T>
+std::ostream &operator<<(std::ostream &stream, const Array<T> &a)
+{
+	for (size_t i = 0; i < 10; i++)
+	{
+		stream << "[" MAGENTA << a[i] << RESET "] ";
+	}
+	stream << "..." << std::endl;
+	return (stream);
+
 };
 
 #endif
