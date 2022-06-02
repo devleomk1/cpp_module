de08dcdee3cb1dd53921856f4e0a9c206bc10755
Kang Jisoon <wltnskjs@naver.com>
2022-06-02 23:25:26 +0900

    ✅ Update CPP08/ex00

--- CPP08/ex00/easyfind.hpp
+++ CPP08/ex00/easyfind.hpp
@@ -6,8 +6,32 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 12:54:36 by jisokang          #+#    #+#             */
-/*   Updated: 2022/06/02 12:55:05 by jisokang         ###   ########.fr       */
+/*   Updated: 2022/06/02 22:28:23 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include <iostream>
+# include <algorithm>	//std::find
+# include <vector>
+# include <exception>
+# include <stdexcept>
+
 template <typename T>
+typename T::iterator easyfind(T &t, int target)
+{
+	/* T는 int 컨테이너라고 가정한다. */
+	/* 찾지 못한 에러 케이스를 생각해야 한다. */
+	typename T::iterator it = std::find(t.begin(), t.end(), target);
+	if (it == t.end())
+		throw std::runtime_error("Target Not Found");
+	return (it);
+
+	//-> find cplusplus.com
+	//iterotr
+}
+
+
+#endif
--- CPP08/ex00/main.cpp
+++ CPP08/ex00/main.cpp
@@ -6,7 +6,38 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/02 12:54:33 by jisokang          #+#    #+#             */
-/*   Updated: 2022/06/02 12:54:34 by jisokang         ###   ########.fr       */
+/*   Updated: 2022/06/02 23:24:24 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+# include "easyfind.hpp"
+/* 어떻게 출력 해야 할까?! */
+#define MAX_ARR		10
+int	main( void )
+{
+	// vector로 데이터를 넣은 통을 만들고 거기서 특정 값을 찾아 보자
+	// vector가 뭔데?
+	//vector<int>v = {1, 2, 3}; 개멍청 push back 하자
+	//std::vector<int> vec = {5, 9, 7, 6, 4, 3, 1, 2, 8, 0};
+	std::vector<int> vec;
+
+	for (size_t i = 0; i < MAX_ARR; i++)
+	{
+		vec.push_back(i);
+	}
+	std::vector<int>::iterator it;
+	try
+	{
+		it = easyfind(vec, 6);
+		std::cout << "easyfind(vec, 6): " << *it << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+
+
+	return (0);
+}
