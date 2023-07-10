f0e409676d0cedf866b4be82de6c2e5e7529b880
Kang Jisoon <wltnskjs@naver.com>
2023-07-10 21:32:50 +0900

    ✨ New.Feat display real time stamp

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -6,23 +6,63 @@
 /*   By: jisokang <jisokang@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 23:44:58 by jisokang          #+#    #+#             */
-/*   Updated: 2023/07/10 16:04:33 by jisokang         ###   ########.fr       */
+/*   Updated: 2023/07/10 19:01:34 by jisokang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <iostream>
+#include <time.h>
 
+/**
+ * @brief static 멤버 변수 초기화
+ *
+ */
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+/**
+ * @brief Construct a new Account:: Account object (사용안함)
+ *
+ */
 Account::Account( void ) {}
 
+/**
+ * @brief Construct a new Account:: Account object
+ *
+ * @param initial_deposit 초기 예치금
+ */
 Account::Account( int initial_deposit )
 {
+	_displayTimestamp();
+
+	this->_accountIndex = _nbAccounts++;
+	this->_amount		= initial_deposit;
+
+	_totalAmount += initial_deposit;
+
+	std::cout
+		<< "index:"   << this->_accountIndex
+		<< ";amount:" << this->_amount
+		<< ";created"  <<
+	std::endl;
 
 }
 
+/**
+ * @brief Destroy the Account:: Account object
+ *
+ */
 Account::~Account( void )
 {
-
+	_displayTimestamp();
+	std::cout
+		<< "index:"   << this->_accountIndex
+		<< ";amount:" << this->_amount
+		<< ";closed"  <<
+	std::endl;
 }
 
 /**
@@ -62,17 +102,25 @@ int	Account::getNbWithdrawals( void )
 
 void	Account::_displayTimestamp( void )
 {
-	std::cout << "[19920104_091532] ";
+	time_t rawtime;
+	struct tm * timeinfo;
+	char buffer [80];
+
+	time (&rawtime);
+	timeinfo = localtime (&rawtime);
+
+	std::strftime (buffer,80,"%Y%m%d_%H%M%S",timeinfo);
+	std::cout << "[" << buffer << "] " ;
 }
 
 void	Account::displayAccountsInfos( void )
 {
 	_displayTimestamp();
 	std::cout
-		<< "accounts:" << getNbAccounts()
-		<< ";total:" << getTotalAmount()
-		<< ";deposits:" << getNbDeposits()
-		<< ";withdrawals:" << getNbWithdrawals()
+		<< "accounts:"		<< getNbAccounts()
+		<< ";total:"		<< getTotalAmount()
+		<< ";deposits:"		<< getNbDeposits()
+		<< ";withdrawals:"	<< getNbWithdrawals()
 	<< std::endl;
 }
 
